ad6b86fb6d356165a0a8dbc1f159df32d232204a
Nabyl Bennouri <nabyl@liveutrahealthy.com>
2019-04-13 00:07:27 -0700

    Add iOS bridge shell

--- ios/RNTensorflowLite.m
+++ ios/RNTensorflowLite.m
@@ -1,5 +1,6 @@
 
 #import "RNTensorflowLite.h"
+#import <React/RCTLog.h>
 
 @implementation RNTensorflowLite
 
@@ -9,5 +10,29 @@
 }
 RCT_EXPORT_MODULE()
 
+RCT_REMAP_METHOD(createImageRecognizer:(NSDictionary*)data,
+                 createImageRecognizerWithResolver:(RCTPromiseResolveBlock)resolve
+                 rejecter:(RCTPromiseRejectBlock)reject)
+{
+    RCTLogInfo(@"createImageRecognizer Worked!");
+    resolve(@"createImageRecognizer Worked!");
+}
+
+RCT_REMAP_METHOD(recognize:(NSDictionary*)data,
+                 recognizeWithResolver:(RCTPromiseResolveBlock)resolve
+                 rejecter:(RCTPromiseRejectBlock)reject)
+{
+    RCTLogInfo(@"recognize Worked!");
+    resolve(@"recognize Worked!");
+}
+
+RCT_REMAP_METHOD(close,
+                 rejecterWithResolver:(RCTPromiseResolveBlock)resolve
+                 rejecter:(RCTPromiseRejectBlock)reject)
+{
+    RCTLogInfo(@"createNativeModules Worked!");
+    resolve(@"createNativeModules Worked!");
+}
 @end
-  
+
+
